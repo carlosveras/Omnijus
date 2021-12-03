4db2ea71dbd6a4985b2677eca9d25c583e57e640
carlosveras <carlos.veras@reglare.com.br>
2021-12-03 17:33:47 -0300

    Rotina diaria Pic Pay

--- ReglareAnotations/RotinaDiariaPicPay.sql
+++ ReglareAnotations/RotinaDiariaPicPay.sql
@@ -1,3 +1,160 @@
+<----- Rotina diaria PicPay --------->
+
+1-Realizar captura com robo da maquina 34 -- 172.13.0.15
+
+2-Pegar email do dia anterior e conferir os processos 
+  da planilha com o que capturamos utilizar a query abaixo
+
+select pro.id as idprocesso
+		,ttp.Numero
+		,pro.IdStatus 
+		,(select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 15) as Desmenbrado
+		,(select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 5) as DisponibilizadoS3
+		,(select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 21) as IntegradoOmnijus
+		,pro.Forum
+		,pro.Vara
+		,pro.Comarca
+  from TermoTribunalProcesso ttp
+		,Processo pro
+where ttp.Numero = pro.Numero
+and ttp.IdSolicitacaoCaptura = pro.IdSolicitacaoCaptura
+and ttp.NomeParte like 'PIC%'
+and ttp.CriadoEm >= '2021-09-01 00:00:00'
+--and (select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 21) = 0
+and pro.Numero in 
+('5018802-22.2021.8.08.0035',
+'5000909-27.2021.8.08.0032',
+'0801943-27.2021.8.19.0083')
+order by ttp.CriadoEm desc
+
+3- Antes de executar o disponibilizarDistribuicao, verificar se 
+   forum, vara e comarca estao com a nomeclatura certa com base
+   no campo orgaojulgador, utilizar a query abaixo:
+
+select pro.id as idprocesso
+		,ttp.Numero
+		,pro.IdStatus 
+		,ttp.CriadoEm
+		,(select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 15) as DMB
+		,(select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 5) as DPZ
+		,(select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 21) as ITG
+        ,pro.Forum
+		,pro.Vara
+		,pro.Comarca
+		,pro.OrgaoJulgador
+  from TermoTribunalProcesso ttp
+		,Processo pro
+where ttp.Numero = pro.Numero
+and ttp.IdSolicitacaoCaptura = pro.IdSolicitacaoCaptura
+and ttp.NomeParte like 'PIC%'
+and ttp.CriadoEm >= '2021-09-01 00:00:00'
+and (select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 21) = 0
+order by ttp.CriadoEm desc
+  
+
+   SE NAO ESTIVER CORRETO OS CAMPOS FORUM VARA E COMARCA,
+   PEGAR PELO ORGAO JULGADOR E DESMEMBRAR O CONTEUDO NOS 3 CAMPOS
+   E FAZER O UPDTE NA BASE.
+
+begin transaction
+
+update Processo
+set 
+	vara = '17º Juizado Especial Cível'
+	,forum = 'Bangu'
+	,comarca = 'Capital'
+where id in (1889138)
+
+commit transaction
+
+begin transaction
+
+update Processo
+set 
+	vara = '12º JUIZADO ESPECIAL CÍVEL'
+	,forum = 'Méier'
+	,comarca = 'Capital'
+where id in (1889138)
+
+commit transaction
+
+--14º Juizado Especial Cível da Regional de Jacarepaguá
+--17º Juizado Especial Cível da Regional de Bangu
+--12º Juizado Especial Cível da Regional do Méier
+JUIZADO ESPECIAL CÍVEL, CRIMINAL E FAZENDA PÚBLICA	NOVA VENÉCIA	NOVA VENÉCIA	Nova Venécia - Juizado Especial Cível, Criminal e Fazenda Pública
+
+1	1889147	0800577-81.2021.8.19.0202	2	15º Juizado Especial Cível	Madureira	Capital	   15º Juizado Especial Cível da Regional de Madureira
+2	1889140	0801186-61.2021.8.19.0203	2	14º Juizado Especial Cível	Jacarepaguá	Capital	   14º Juizado Especial Cível da Regional de Jacarepaguá
+3	1889138	0803011-37.2021.8.19.0204	2	17º Juizado Especial Cível	Bangu	    Capital	   17º Juizado Especial Cível da Regional de Bangu
+4	1889141	0807631-14.2021.8.19.0036	2	1º JUIZADO ESPECIAL CÍVEL	NILÓPOLIS	NILÓPOLIS	1º Juizado Especial Cível da Comarca de Nilópolis
+5	1889139	5007309-63.2021.8.08.0030	2	2º JUIZADO ESPECIAL CÍVEL	LINHARES	LINHARES	Linhares - 2º Juizado Especial Cível
+
+
+
+
+
+<------------------------------------->
+
+0- ANTES DE RODAR O DISPONIBILIZARDISTRIBUICAO VERIFICAR SE FORUM VARA E COMARCA ESTA CORRETOS!!
+
+select pro.id		
+	,pro.Numero
+	,pro.IdStatus
+	,pro.Vara
+	,pro.Forum
+	,pro.Comarca
+	,pro.OrgaoJulgador
+  from processo pro
+where pro.numero in 
+('0800577-81.2021.8.19.0202',
+'0801186-61.2021.8.19.0203',
+'5007309-63.2021.8.08.0030',
+'0807631-14.2021.8.19.0036')
+
+
+4- SE NAO ESTIVER CORRETO OS CAMPOS FORUM VARA E COMARCA,
+   PEGAR PELO ORGAO JULGADOR E DESMEMBRAR O CONTEUDO NOS 3 CAMPOS
+   E FAZER O UPDTE NA BASE.
+
+begin transaction
+
+update Processo
+set 
+	vara = '17º Juizado Especial Cível'
+	,forum = 'Bangu'
+	,comarca = 'Capital'
+where id in (1889138)
+commit transaction
+--14º Juizado Especial Cível da Regional de Jacarepaguá
+--17º Juizado Especial Cível da Regional de Bangu
+JUIZADO ESPECIAL CÍVEL, CRIMINAL E FAZENDA PÚBLICA	NOVA VENÉCIA	NOVA VENÉCIA	Nova Venécia - Juizado Especial Cível, Criminal e Fazenda Pública
+
+1	1889147	0800577-81.2021.8.19.0202	2	15º Juizado Especial Cível	Madureira	Capital	   15º Juizado Especial Cível da Regional de Madureira
+2	1889140	0801186-61.2021.8.19.0203	2	14º Juizado Especial Cível	Jacarepaguá	Capital	   14º Juizado Especial Cível da Regional de Jacarepaguá
+3	1889138	0803011-37.2021.8.19.0204	2	17º Juizado Especial Cível	Bangu	    Capital	   17º Juizado Especial Cível da Regional de Bangu
+4	1889141	0807631-14.2021.8.19.0036	2	1º JUIZADO ESPECIAL CÍVEL	NILÓPOLIS	NILÓPOLIS	1º Juizado Especial Cível da Comarca de Nilópolis
+5	1889139	5007309-63.2021.8.08.0030	2	2º JUIZADO ESPECIAL CÍVEL	LINHARES	LINHARES	Linhares - 2º Juizado Especial Cível
+
+Cariacica - Comarca da Capital - 4º Juizado Especial Cível
+Vila Velha - Comarca da Capital - 2º Juizado Especial Cível
+
+
+begin transaction
+
+update Processo
+set 
+	vara = '26º JUIZADO ESPECIAL CÍVEL'
+	,forum = 'CAMPO GRANDE'
+	,comarca = 'CAPITAL'
+where id in (1889138)
+commit transaction
+
+26º Juizado Especial Cível da Regional de Campo Grande
+
+
+
+
+
 
 
 1- CONFERIR PLANILHA VINDA DA OITO PARA CONFRONTAR CAPTURAS COM O QUE FOI CAPTURADO 
@@ -24,29 +181,30 @@ and ttp.NomeParte like 'PIC%'
 and ttp.CriadoEm >= '2021-09-01 00:00:00'
 --and (select count(1) from HistoricoStatusProcesso hsp where hsp.idprocesso = pro.Id and hsp.IdStatus = 21) = 0
 and pro.Numero in 
-('0834083-55.2021.8.19.0038',
-'0834055-87.2021.8.19.0038',
-'0803011-37.2021.8.19.0204',
-'0807576-63.2021.8.19.0036',
-'0808011-24.2021.8.19.0008',
-'0801061-93.2021.8.19.0203',
-'0802525-40.2021.8.19.0208')
+(
+'0800577-81.2021.8.19.0202',
+'0801186-61.2021.8.19.0203',
+'5007309-63.2021.8.08.0030',
+'0807631-14.2021.8.19.0036')
+
 order by ttp.CriadoEm desc
 
 
 2- TIRAR OS ESPACOS EM BRANCO DO FORUM VARA E COMARCA
+
+begin transaction
 update processo
    set vara = trim(vara)
 		,forum = trim(forum)
 		,comarca = trim(comarca)
 where numero in (
-('0834083-55.2021.8.19.0038',
-'0834055-87.2021.8.19.0038',
-'0803011-37.2021.8.19.0204',
-'0807576-63.2021.8.19.0036',
-'0808011-24.2021.8.19.0008',
-'0801061-93.2021.8.19.0203',
-'0802525-40.2021.8.19.0208')
+(
+'0800577-81.2021.8.19.0202',
+'0801186-61.2021.8.19.0203',
+'5007309-63.2021.8.08.0030',
+'0807631-14.2021.8.19.0036')
+commit transaction
+
 
 3- QUERY PARA VALIDAR SE FORUM VARA E COMARCA ESTAO CORRETOS
 
@@ -58,14 +216,11 @@ select pro.id
 	,pro.Comarca
 	,pro.OrgaoJulgador
   from processo pro
-where pro.numero in (
-('0834083-55.2021.8.19.0038',
-'0834055-87.2021.8.19.0038',
-'0803011-37.2021.8.19.0204',
-'0807576-63.2021.8.19.0036',
-'0808011-24.2021.8.19.0008',
-'0801061-93.2021.8.19.0203',
-'0802525-40.2021.8.19.0208')
+where pro.numero in 
+('0800577-81.2021.8.19.0202',
+'0801186-61.2021.8.19.0203',
+'5007309-63.2021.8.08.0030',
+'0807631-14.2021.8.19.0036')
 
 
 4- SE NAO ESTIVER CORRETO OS CAMPOS FORUM VARA E COMARCA,
@@ -80,18 +235,18 @@ set
 	,forum = 'Bangu'
 	,comarca = 'Capital'
 where id in (1889138)
-
+commit transaction
 --14º Juizado Especial Cível da Regional de Jacarepaguá
 --17º Juizado Especial Cível da Regional de Bangu
+--Cariacica - Comarca da Capital - 2º Juizado Especial Cível
 
-
-1	1889147	0800577-81.2021.8.19.0202	2	15º Juizado Especial Cível	Madureira	Capital	15º Juizado Especial Cível da Regional de Madureira
-2	1889140	0801186-61.2021.8.19.0203	2	14º Juizado Especial Cível	Jacarepaguá	Capital	14º Juizado Especial Cível da Regional de Jacarepaguá
-3	1889138	0803011-37.2021.8.19.0204	2	17º Juizado Especial Cível	Bangu	Capital	17º Juizado Especial Cível da Regional de Bangu
+1	1889147	0800577-81.2021.8.19.0202	2	15º Juizado Especial Cível	Madureira	Capital	   15º Juizado Especial Cível da Regional de Madureira
+2	1889140	0801186-61.2021.8.19.0203	2	14º Juizado Especial Cível	Jacarepaguá	Capital	   14º Juizado Especial Cível da Regional de Jacarepaguá
+3	1889138	0803011-37.2021.8.19.0204	2	17º Juizado Especial Cível	Bangu	    Capital	   17º Juizado Especial Cível da Regional de Bangu
 4	1889141	0807631-14.2021.8.19.0036	2	1º JUIZADO ESPECIAL CÍVEL	NILÓPOLIS	NILÓPOLIS	1º Juizado Especial Cível da Comarca de Nilópolis
 5	1889139	5007309-63.2021.8.08.0030	2	2º JUIZADO ESPECIAL CÍVEL	LINHARES	LINHARES	Linhares - 2º Juizado Especial Cível
 
-commit transaction
+
 
 
 
